d2111539609009c7e0678fb6e1786578446a5b26
zoeferencova <zoeferencova@gmail.com>
2023-01-19 17:13:03 +0900

    added hamburger functionality to mobile menu

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
 import logo from '../images/logo.png'
 
 const Header = ({ firstPath }) => {
-    console.log(firstPath)
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+    const hamburgerLine = `h-[3px] w-5 mb-1 rounded-full bg-black transition ease transform duration-300`;
     return (
         <div className="px-10 py-8 sm:px-20 ">
-            <div className="w-full py-8 flex justify-between items-center">
-                <div>
-                    <Link href='/'>
-                        <Image src={logo} alt='tada blog' width='95' />
-                    </Link>
-                </div>
-                <div className='inline-block cursor-pointer sm:hidden'>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                </div>
-                <div className='hidden sm:flex sm:items-center header-links'>
-                    <Link href='/' className={`${(firstPath === '' || firstPath === 'category') && 'header-nav-current'} header-nav-item`}>Blog</Link>
-                    <Link href='/learning' className={`${firstPath === 'learning' && 'header-nav-current'} header-nav-item`}>Learning</Link>
-                    <Link href='/resources' className={`${firstPath === 'resources' && 'header-nav-current'} header-nav-item`}>Resources</Link>
+            <div className="w-full pt-8 pb-3 sm:pb-8 flex flex-wrap justify-between items-center">
+                <Link href='/'>
+                    <Image src={logo} alt='tada blog' width='95' />
+                </Link>
+                <button
+                    className="flex flex-col h-6 w-6 group cursor-pointer sm:hidden"
+                    onClick={() => setHamburgerOpen(!hamburgerOpen)}
+                >
+                    <div className={`${hamburgerLine} ${hamburgerOpen && "rotate-45 translate-y-[7px]"}`} />
+                    <div className={`${hamburgerLine} ${hamburgerOpen && "opacity-0"}`} />
+                    <div className={`${hamburgerLine} ${hamburgerOpen && "-rotate-45 -translate-y-[7px]"}`} />
+                </button>
+                <div className={`${!hamburgerOpen && 'opacity-0'} ${hamburgerOpen && 'opacity-100 transition ease duration-300 flex-col items-end'} flex h-7 w-full sm:w-auto sm:opacity-100 sm:flex sm:items-center header-links`}>
+                    <Link href='/' onClick={() => setHamburgerOpen(false)} className={`${(firstPath === '' || firstPath === 'category') && 'header-nav-current'} ${hamburgerOpen && 'mb-2'} header-nav-item`}>Blog</Link>
+                    <Link href='/learning' onClick={() => setHamburgerOpen(false)} className={`${firstPath === 'learning' && 'header-nav-current'} ${hamburgerOpen && 'mb-2'} header-nav-item`}>Learning</Link>
+                    <Link href='/resources' onClick={() => setHamburgerOpen(false)} className={`${firstPath === 'resources' && 'header-nav-current'}  header-nav-item`}>Resources</Link>
                 </div>
             </div>
         </div>
